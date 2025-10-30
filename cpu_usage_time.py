febe719d5e6ae835821e238902acb14433a27ed3
Luis Enrique Jimenez Loa <luisloa.26@outlook.com>
2025-10-30 11:02:52 -0600

    Update cpu_usage_time.py

--- cpu_usage_time.py
+++ cpu_usage_time.py
@@ -6,19 +6,26 @@ import pandas as pd
 
 
 def fetch_cpu_request_utilization(project_id: str, minutes: int = 10):
+    # Instacia cliente para API Monitoring
     client = monitoring_v3.MetricServiceClient()
+    
+    # Formato requerido para la API
     project_name = f"projects/{project_id}"
 
     interval = monitoring_v3.TimeInterval({
         "end_time": datetime.now(timezone.utc),
         "start_time": datetime.now(timezone.utc) - timedelta(minutes=minutes)
     })
+    #-----------------------start_time----------------------|end_time
+    #                             -------------------------->
 
+    # Filtro para obtener la utilizacion de CPU por contenedor
     filter_str = (
         'metric.type="kubernetes.io/container/cpu/request_utilization" '
         'AND resource.type="k8s_container" '
     )
 
+    # Solicitud a la API con los parametro definidos
     results = client.list_time_series(request={
         "name": project_name,
         "filter": filter_str,
@@ -26,19 +33,25 @@ def fetch_cpu_request_utilization(project_id: str, minutes: int = 10):
         "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL
     })
 
-    # Convert each TimeSeries to a dictionary
-    #series_list = []
+    # Datos en bruto
     #for series in results:
-    #    series_dict = MessageToDict(series._pb)  # Access protobuf object
-    #    series_list.append(series_dict)
+    #    for point in series.points:
+    #        container = series.resource.labels.get("container_name")
+    #        pod = series.resource.labels.get("pod_name")
+    #        print(container, pod, point.value.double_value)
 
+    #print(type(results))
+    
+    # Retorna un google.cloud.monitoring_v3.services.metric_service.pagers.ListTimeSeriesPager
     return results
 
-def build_dataframe(time_series):
+# Convierte ListTimeSeries a DataFrame
+def build_dataframe(list_time_series):
     import pandas as pd
 
+    # Genera una list de dics con los valores de TimeSeries requeridos
     rows = []
-    for series in time_series:
+    for series in list_time_series:
         resource = series.resource.labels
         for point in series.points:
             rows.append({
@@ -50,17 +63,17 @@ def build_dataframe(time_series):
                 "utilization": point.value.double_value
             })
 
+    # Covierte a DataFrame
     df = pd.DataFrame(rows)
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     return df
 
 def analyze_cpu_utilization(df, threshold=0.1):
-    # Opción 1: Promedio de utilización por contenedor
+
     avg_by_container = df.groupby("container")["utilization"].mean().sort_values(ascending=False)
 
-    # Opción 3: Contenedores con puntos por debajo del umbral
     underutilized = df[df["utilization"] < threshold]
-    underutilized_counts = underutilized.groupby("container").size().sort_values(ascending=False)
+    underutilized_counts = (underutilized.groupby("container").size().sort_values(ascending=False))
 
     return avg_by_container, underutilized_counts
 
@@ -76,7 +89,7 @@ if __name__ == "__main__":
     print(avg)
 
     print("\n- Contenedores con puntos por debajo del 10%:")
-    print(underutilized)
+    print(f'{underutilized}%')
 
 
     
